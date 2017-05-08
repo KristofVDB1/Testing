a9a2964246b27bf7b76ced8b612ada496d444308
Bart Huygen <bart.huygen@student.ucll.be>
2017-05-08 15:42:54 +0200

    Story 02 - Punt afgewerkt (Bart: pc, Kristof en Krijn: mondeling)

--- GamesuiteHangman/src/ui/Launcher.java
+++ GamesuiteHangman/src/ui/Launcher.java
@@ -1,16 +1,21 @@
 package ui;
 
 import javax.swing.JOptionPane;
-
+import ui.PictionaryUi;
 import domain.Speler;
 
 public class Launcher {
 
 	public static void main(String[] args) {
 		String naam = JOptionPane.showInputDialog("Welkom! \nHoe heet je?");
-		Speler speler = new Speler(naam);
+		//Speler speler = new Speler(naam);
 
-		JOptionPane.showMessageDialog(null, "... zal binnekort spelen", speler.getNaam(), JOptionPane.INFORMATION_MESSAGE);
+		//JOptionPane.showMessageDialog(null, "... zal binnekort spelen", speler.getNaam(), JOptionPane.INFORMATION_MESSAGE);
+		
+		PictionaryUi hello = new PictionaryUi();
+		hello.showMenu();
+
+		
 	}
 
 }
---
+++ GamesuiteHangman/src/ui/PictionaryUi.java
@@ -0,0 +1,35 @@
+package ui;
+
+import javax.swing.JOptionPane;
+
+import domain.Punt;
+import domain.Speler;
+
+public class PictionaryUi {
+	
+	private Speler speler;
+	
+	
+	
+	public PictionaryUi(){
+		//setSpeler(speler);
+	}
+	
+	
+	public void setSpeler(Speler speler) {
+		this.speler = speler;
+	}
+
+	
+	
+	public void showMenu(){
+		int x = Integer.parseInt(JOptionPane.showInputDialog("x coördinaat van het punt"));
+		int y = Integer.parseInt(JOptionPane.showInputDialog("y coördinaat van het punt"));
+		
+		Punt punt = new Punt(x, y);
+		
+		JOptionPane.showMessageDialog(null, "U heeft een correct punt aangemaakt: " + punt.toString());
+		
+	}
+	
+}
