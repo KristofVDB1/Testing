3e4c88cf58669733e8dd697b1d3402323a9ce2b2
MilanW <milan.wauters@student.ucll.be>
2017-05-09 10:35:02 +0200

    Story01: klasse speler (denker: oliver, doener: milan)

--- GamesuiteHangman/src/domain/Speler.java
+++ GamesuiteHangman/src/domain/Speler.java
@@ -1,5 +1,49 @@
 package domain;
 
 public class Speler {
-
+	private String naam;
+	private int score;
+	
+	public Speler(String naam){
+	//	super();
+		this.setNaam(naam);
+		this.score = 0;
+	}
+	
+	public void setNaam(String naam) {
+		if(naam == null || naam.isEmpty()){
+		//	throw new DomainException("ongeldige naam");
+		}
+		this.naam = naam;
+	}
+	
+	public String getNaam() {
+		return naam;
+	}
+	
+	public int getScore() {
+		return score;
+	}
+	
+	public void addToScore(int score){
+		if(this.score + score < 0){
+			throw new IllegalArgumentException("score wordt negatief");
+		}
+		this.score = this.score + score;
+	}
+	
+	public boolean equals(Speler speler){
+		if(this.naam != speler.getNaam() || this.score != speler.getScore() || speler == null){
+			return false;
+		}
+		else{
+			return true;
+		}
+	}
+	
+	public String toString(){
+		return "naam: " + getNaam() + " score: " + getScore();
+	}
+	
+	
 }
