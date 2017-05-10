b9bc107da03d01d8f236b4cb947ae030b3e4e70a
KristofVandenbroucke <Kristof.vandenbrouck@gmail.com>
2017-05-10 10:10:26 +0200

    Milan PC, Kristof Denker, equals methode afgemaakt van lijnstuk

--- GamesuiteHangman/src/domain/Lijnstuk.java
+++ GamesuiteHangman/src/domain/Lijnstuk.java
@@ -1,26 +1,25 @@
 package domain;
 
-public class LijnStuk extends Vorm{
-	
+public class LijnStuk extends Vorm {
+
 	private Punt start;
 	private Punt eind;
-	
-	
-	public LijnStuk(Punt start, Punt eind){
+
+	public LijnStuk(Punt start, Punt eind) {
 		setStartEnEindPunt(start, eind);
-		
+
 	}
 
 	public void setStartEnEindPunt(Punt start, Punt eind) {
-		if (start != null && eind != null && start != eind){
+		if (start != null && eind != null && start != eind) {
 			this.start = start;
 			this.eind = eind;
-		}else{
+		} else {
 			throw new DomainException("Ongeldige punten");
 		}
-		
+
 	}
-	
+
 	public Punt getEindPunt() {
 		return eind;
 	}
@@ -28,26 +27,28 @@ public class LijnStuk extends Vorm{
 	public Punt getStartPunt() {
 		return start;
 	}
-	
+
 	public boolean equals(Object o) {
-		if(o == null){
+		if (o == null) {
 			return false;
 		}
-		
-		Punt start1 = ((LijnStuk) o).getStartPunt();
-		Punt eind1 = ((LijnStuk) o).getEindPunt();
-		
-		if ((start1.getX() == this.start.getX() && start1.getY() == this.start.getY()) &&
-				(eind1.getX() == this.eind.getX() && eind1.getY() == this.eind.getY())){
+
+		if (o instanceof LijnStuk) {
+
+			Punt start1 = ((LijnStuk) o).getStartPunt();
+			Punt eind1 = ((LijnStuk) o).getEindPunt();
+
+			if ((start1.getX() == this.start.getX() && start1.getY() == this.start.getY())
+					&& (eind1.getX() == this.eind.getX() && eind1.getY() == this.eind.getY())) {
 				return true;
+			}
 		}
 		return false;
 	}
 
 	@Override
 	public String toString() {
-		return "Lijnstuk met als startpunt: " + start +" en als eindpunt: " + eind;
+		return "Lijnstuk met als startpunt: " + start + " en als eindpunt: " + eind;
 	}
-	
-	
+
 }
